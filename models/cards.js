0883269628292694a7e166912eb50b95af3bfa7a
Sergei Petrov <seevpetrov@gmail.com>
2022-08-28 22:44:01 +0300

    feat: создана схемы пользователей и карточек

--- models/cards.js
+++ models/cards.js
@@ -13,8 +13,22 @@ const cardSchema = new mongoose.Schema({
   },
   owner: {
     type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
     required: true,
+  },
+  likes: {
+    type: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user',
+      },
+    ],
+    default: [],
+  },
+  createdAt: {
+    type: mongoose.Schema.Types.Date,
+    default: Date.now,
   },
 });
 
-module.exports = mongoose.model('card', userSchema);
+module.exports = mongoose.model('card', cardSchema);
